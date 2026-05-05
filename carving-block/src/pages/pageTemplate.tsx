3c3e9bce1b5072eda9405edf17f0ebf859fcdba8
NeemZ16 <neemzam2@gmail.com>
2026-05-05 15:48:57 -0400

    fixed build type and unused errors

--- carving-block/src/pages/pageTemplate.tsx
+++ carving-block/src/pages/pageTemplate.tsx
@@ -2,7 +2,6 @@
 import Header from '../components/header';
 import WalletDetails from '../components/walletDetails';
 import YourBookings from '../components/yourBookings';
-import type { ProjectListingProps } from '../components/projListing';
 
 type PageProps = {
   ptitle: string;
@@ -10,40 +9,6 @@ type PageProps = {
 };
 
 function Page({ ptitle, children }: PageProps) {
-
-  const listings: ProjectListingProps[] = [
-    {
-      id: "2",
-      title: "Item with no description",
-      price: 500,
-      duration: 90,
-      image: "",
-      booked: true,
-      time: new Date(),
-      state: 1,
-    },
-    {
-      id: "3",
-      title: "Item with description",
-      price: 500,
-      duration: 90,
-      image: "",
-      booked: true,
-      time: new Date(),
-      description: "Required tools: A B C D E F. Difficulty level: beginner?? fih.",
-      state: 1,
-    },
-    {
-      id: "4",
-      title: "Whittled Bear",
-      price: 250,
-      duration: 30,
-      image: "",
-      booked: true,
-      time: new Date(),
-      state: 1,
-    },
-  ]
 
   return (
     <>
@@ -57,7 +22,7 @@ function Page({ ptitle, children }: PageProps) {
         <aside>
           <WalletDetails />
           <hr className='dashed' />
-          <YourBookings bookings={listings} />
+          <YourBookings />
         </aside>
       </div>
     </>
